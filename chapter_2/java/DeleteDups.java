32c9ec9e1ebcc21814313ae294cbf48a4ea73b09
Danlan Chen <danlanchenchen@gmail.com>
2017-08-05 14:20:53 -0400

    Update DeleteDups.java

--- chapter_2/java/DeleteDups.java
+++ chapter_2/java/DeleteDups.java
@@ -12,4 +12,18 @@ class DeleteDups{
             n = n.next
         }
     }
+    void deleteDupsWithNoBuffer(LinkedListNode head){
+        LinkedListNode current = head;
+        while (current != null){
+            LinkedListNode runner = current;
+            while (runner.next! = null){
+                if(runner.next.data == current.data){
+                    runner.next = runner.next.next;
+                }else{
+                    runner = runner.next; 
+                }
+            }  
+            current = current.next;
+        }
+    }
 }
