dc1d092e0108723af959d428c8bf9c7ed72e1d2e
Danlan Chen <danlanchenchen@gmail.com>
2017-08-05 19:10:26 -0400

    Create bfs.java

---
+++ chapter_4/java/bfs.java
@@ -0,0 +1,18 @@
+class BFS{
+    void search(Node root){
+        Queue queue = new Queue();
+        root.marked = true;
+        queue.enqueue(root);
+        
+        while(!queue.isEmpty()){
+            Node r = queue.dequeue();
+            visit(r);
+            foreach(Node n in r.adjacent){
+                if (n.marked == false){
+                     n.marked = true;
+                     queue.enqueue(n);
+                }
+            }
+        }
+    }
+}
